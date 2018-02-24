2d1ae37837a19358156470d2793944ad21cec3e6
Harshad Maniyar <harshad.maniyar@atos.net>
2018-02-24 17:29:23 +0530

    change in login and user model

--- BIOSclient/src/app/components/login/login.component.ts
+++ BIOSclient/src/app/components/login/login.component.ts
@@ -12,29 +12,34 @@ import { UtilFunctions } from '../../utils/utilFunctions';
 export class LoginComponent implements OnInit {
 
   submitted = false;
-  user: User;
-  loginFormObj:any = {};
+  // user: User;
+  loginFormObj: User;
+  errorObj:any = {};
   constructor(private router: Router, private _usrSer: UserService) {
-    this.user = new User();
+    this.initializeUser();
    }
 
   ngOnInit() {
     UtilFunctions.clearLocalStorage();
   }
 
+  initializeUser(){
+    this.loginFormObj = new User();
+  }
  loginUser() {
    if(Object.keys(this.loginFormObj).length <= 0) {
       return false;
    }
-   if (this.loginFormObj.userName == 'horst' && this.loginFormObj.password == 'abc123') {
+   if (this.loginFormObj.loginName == 'horst' && this.loginFormObj.userPassword == 'abc123') {
       UtilFunctions.setLocalStorage('userDetail','horst');
       this.router.navigate(['/dashboard']);
    } else {
-    this.loginFormObj = {};
-     this.loginFormObj.loginFormError = {
+    this.initializeUser();
+     this.errorObj = {
       hasError: true,
       errorMsg: 'Username or Password is incorrect'
      };
+
    }
   //  this._usrSer.loginUser(data).then((res) => {
   //   if (res.Status) {
