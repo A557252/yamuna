b585a7de4b1713dee6c5d70244fe8e13863951ff
Horst <horst.2.richter@atos.net>
2018-02-26 14:12:44 +0100

    UI changes

--- BIOSclient/src/app/components/main/inquiries/inquiries.component.ts
+++ BIOSclient/src/app/components/main/inquiries/inquiries.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Broadcaster } from '../../../utils/brodcaster';
+import { Model } from '../../share/model.model';
 
 @Component({
   selector: 'app-inquiries',
@@ -11,12 +12,18 @@ export class InquiriesComponent implements OnInit {
   inquiriesArray: any = [];
 
   constructor(private broadcaster: Broadcaster) { }
-
+  model: Model;
+  
   ngOnInit() {
     this.broadcaster.on<string>('updateInquiries')
     .subscribe(message => {
       console.log('updateInquiries'+message);
       this.inquiriesArray = message;
+    });
+    this.broadcaster.on<string>('modelData')
+    .subscribe((message: any) => {
+      this.model = message;
+      console.log(this.model);
     });
   }
 
